e61b809291727b158c90c5faa8ce3f8eaff7de81
dreevo <sidedark85@gmail.com>
2023-03-26 05:01:56 +0100

    first commit

---
+++ src/main/java/com/polarbookshop/edgeservive/EdgeServiveApplication.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.edgeservive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeServiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeServiveApplication.class, args);
+	}
+
+}
